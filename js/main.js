96aa3e2281314011f3982712683902d49efaee9e
avaz-rustamov <avaz.dev@icloud.com>
2022-02-11 14:48:31 +0500

    Update js code

--- js/main.js
+++ js/main.js
@@ -2,19 +2,26 @@ let elForm = document.querySelector(".form-js");
 var elFormInput = document.querySelector(".fizzbuzz-input");
 var elResult = document.querySelector(".form-result");
 
+const fizzBuzzCheck = function (num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    return "Fizz";
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  } else return num;
+};
+
 elForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
 
   let inputValue = elFormInput.value;
 
-  if (inputValue % 3 === 0 && inputValue % 5 === 0) {
-    elResult.textContent = "FizzBuzz";
-    console.log("FizzBuzz");
-  } else if (inputValue % 3 === 0) {
-    elResult.textContent = "Fizz";
-    console.log("Fizz");
-  } else if (inputValue % 5 === 0) {
-    elResult.textContent = "Buzz";
-    console.log("Buzz");
-  } else return inputValue;
+  if (inputValue <= 0 || isNaN(inputValue)) {
+    return (elResult.textContent = `Enter number between 1 and 10.000`);
+  }
+
+  elResult.textContent = fizzBuzzCheck(inputValue);
 });
